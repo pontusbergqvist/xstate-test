f10e10f16654a6ddd737f507eaafbc0e84cee991
Pontus Bergqvist <ponber@ponber-laptop.sector5.local>
2023-01-18 10:23:51 +0100

    added create + delete

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,39 @@
 import { useMachine } from "@xstate/react";
 import { machine } from "../machines/machine";
 
+let todos = ["Sak 1", "Sak 2", "Sak 3", "Sak 4"]
+
 export default function Home() {
   const [state, send] = useMachine(machine, {
     services: {
       loadTodos: async () => {
-        // throw new Error("veri bad error");
-        return ["Sak 1", "Sak 2"];
+        return Array.from(todos);
       },
+      saveTodo: async (ctx, event) => {
+        todos = [...todos, ctx.newTodoInput];
+      },
+      deleteTodo: async (ctx, event) => {
+        todos = todos.filter((item, i) => event.value.id != i)
+      }
     },
   });
   return (
     <div>
       <p>{JSON.stringify(state.value)}</p>
-      <p>{JSON.stringify(state.context)}</p>
+      <ul>
+        {state.context.todos.map((item, index) => (
+          <li id={index} key={index} onClick={(event) => send({ type: "Delete", value: event.target })}>{item}</li>
+        ))}
+      </ul>
       {state.matches("Todos are loaded") && (
         <button onClick={() => send({ type: "Create new" })}>Create new</button>
       )}
       {state.matches("Creating new todo") && (
-        <input type="text" onChange={(e) => send({type: "Form input changed", value: e.target.value})} />
+        <div>
+          <input type="text" onKeyDown={(e) => e.key === "Enter" && send("Submit")} onChange={(e) => send({type: "Form input changed", value: e.target.value})} />
+          <br /> 
+          <button onClick={() => send("Submit")}>Submit todo</button>
+        </div>
       )}
     </div>
   );
